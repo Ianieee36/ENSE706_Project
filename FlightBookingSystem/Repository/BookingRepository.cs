b3cc34b635571dbf4299f75d39f071d515456457
Ianieee36 <colitos169@gmail.com>
2026-05-06 09:54:58 +1200

    Added: BookingRepository.cs
    I've implemented some of the methods for retrieving, saving, or deleting data for bookings.

--- FlightBookingSystem/Repository/BookingRepository.cs
+++ FlightBookingSystem/Repository/BookingRepository.cs
@@ -8,17 +8,77 @@ namespace FlightBookingSystem.Repository
     {
         public Booking? FindBookingsByUserId(string userId)
         {
-            // -- to be implemented
+            using OracleConnection conn = dbConnection.GetConnection();
+            conn.Open();
+
+            string query = "SELECT * FROM BOOKINGS WHERE USERID :userId";
+
+            using OracleCommand cmd = new OracleCommand(query, conn);
+            cmd.Parameters.Add(new OracleParameter("userId", userId));
+
+            using OracleDataReader reader = cmd.ExecuteReader();
+
+            if (reader.read())
+            {
+                BookingStatus status = Enum.Parse<BookingStatus>(reader["BOOKINGSTATUS"].ToString()!);
+
+                return new Booking (
+                    reader["BOOKINGID"].ToString()!,
+                    Convert.ToDateTime(reader["BOOKINGDATE"]),
+                    reader["USERID"].ToString()!,
+                    reader["FLIGHTID"].ToString()!
+                    
+                );
+            }
+            return null;
         }
 
         public Booking? FindBookingsByFlightId(string flightId)
         {
-            // -- to be implemented
+            using OracleConnection conn = dbConnection.GetConnection();
+            conn.Open();
+
+            string query = "SELECT * FROM BOOKINGS WHERE FLIGHTID :flightId";
+
+            using OracleCommand cmd = new OracleCommand(query, conn);
+            cmd.Parameters.Add(new OracleParameter("flightId", flightId));
+
+            using OracleDataReader reader = cmd.ExecuteReader();
+
+            if (reader.read())
+            {
+                BookingStatus status = Enum.Parse<BookingStatus>(reader["BOOKINGSTATUS"].ToString()!);
+
+                return new Booking (
+                    reader["BOOKINGID"].ToString()!,
+                    Convert.ToDateTime(reader["BOOKINGDATE"]),
+                    reader["USERID"].ToString()!,
+                    reader["FLIGHTID"].ToString()!
+                    
+                );
+                
+            }
+            return null;
         }
 
         public void SaveBookings(Booking bookings)
         {
-            // -- to be implemented
+            using OracleConnection conn = dbConnection.GetConnection();
+            conn.Open();
+
+            string query = @"INSERT INTO BOOKINGS (BOOKINGID, BOOKINGDATE, STATUS, USERID, FLIGHTID)
+                             VALUES (:bookingId :bookingDate :status :userId :flightId)";
+
+            using OracleCommand cmd = new OracleCommand(query, conn);
+
+            cmd.Parameters.Add(new OracleParameter("bookingId", bookings.BookingId));
+            cmd.Parameters.Add(new OracleParameter("bookingDate", bookings.BookingDate));
+            cmd.Parameters.Add(new OracleParameter("status", bookings.BookingStatus.ToString()));
+            cmd.Parameters.Add(new OracleParameter("bookingId", bookings.BookingId));
+            cmd.Parameters.Add(new OracleParameter("userId", bookings.User.UserId));
+            cmd.Parameters.Add(new OracleParameter("flightId", bookings.Flight.FlightId));
+
+            cmd.ExecuteNonQuery();
         }
 
         public void DeleteBooking(string userId)
