7ca141778d38a138eec3efee9151ace29063f657
Ianieee36 <colitos169@gmail.com>
2026-05-15 23:44:33 +1200

    Update: UserService.cs
    User service implemented with proper comments.

--- FlightBookingSystem0.2/FlightBookingSystem/Services/UserService.cs
+++ FlightBookingSystem0.2/FlightBookingSystem/Services/UserService.cs
@@ -1,23 +1,68 @@
 using System;
 using FlightBookingSystem.Model;
+using FlightBookingSystem.Repository;
 
 namespace FlightBookingSystem.Services
 {
     internal class UserService : IUserService
     {
-        public User Login(string email, string password)
+        private readonly IUserRepository userRepository; // interface dependency 
+
+        public UserService()
         {
-            throw new NotImplementedException();
+            userRepository = new UserRepository(); // instantiate the UserRepository class
         }
 
-        public User Register(string email, string password, Role role)
+        public User? Login(string email, string password)
         {
-            throw new NotImplementedException();
+            User? user = userRepository.FindUserByEmail(email); // search email through the database
+
+            if(user == null) // checks if user email exists 
+            {
+                return null;
+            }
+
+            if(user.PasswordHash != password) // checks if the stored password matches the entered password.
+            {
+                return null;
+            }
+
+            return user; // return user
         }
 
-        public void Logout(string userId)
+        public User? Register(string email, string password, Role role, string firstName, 
+                             string lastName, DateTime dateOfBirth, 
+                             string address, string phoneNumber)
         {
+            User? existingUser = userRepository.FindUserByEmail(email); // checks DB with any existing emails
+
+            if(existingUser != null) // checks if theres existing email
+            {
+                return null; 
+            }
+
+            string userId = Guid.NewGuid().ToString(); // Generate ID
+
+            User newUser = new User( // creates a new User
+                userId,
+                email,
+                password,
+                role,
+                firstName,
+                lastName,
+                dateOfBirth,
+                address,
+                phoneNumber
+            );
+
+            userRepository.SaveUser(newUser); // saves newUser to DB
             
+            return newUser;
+        }
+
+        public User? Logout()
+        {
+            return null; // logouts user;
         }
     }
 }
