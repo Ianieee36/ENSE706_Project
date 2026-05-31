e82b968e2aa8bc0c6a99e785e35529d91f736d2d
Ianieee36 <colitos169@gmail.com>
2026-05-31 23:23:30 +1200

    Update: UserFactory.cs
    Needed to update my user factory so that it matches the constructor inside domain class.

--- FlightBookingSystem0.2/Factory/UserFactory.cs
+++ FlightBookingSystem0.2/Factory/UserFactory.cs
@@ -3,36 +3,36 @@ using FlightBookingSystem.Model;
 namespace FlightBookingSystem.Factory
 {
     public static class UserFactory
-{
-    public static Customer CreateCustomer(
-        string userId,
-        string email,
-        string passwordHash,
-        string firstName,
-        string lastName,
-        DateTime dateOfBirth,
-        string address,
-        string phoneNumber,
-        int loyaltyPoints,
-        string membershipTier)
     {
-        return new Customer(userId, email, passwordHash, firstName, lastName,
-            dateOfBirth, address, phoneNumber, loyaltyPoints, membershipTier);
-    }
+        public static Customer CreateCustomer(
+            string userId,
+            string email,
+            string passwordHash,
+            string firstName,
+            string lastName,
+            DateTime dateOfBirth,
+            string address,
+            string phoneNumber,
+            int loyaltyPoints,
+            string membershipTier)
+        {
+            return new Customer(userId, email, passwordHash, firstName, lastName,
+                dateOfBirth, address, phoneNumber);
+        }
 
-    public static Admin CreateAdmin(
-        string userId,
-        string email,
-        string passwordHash,
-        string firstName,
-        string lastName,
-        DateTime dateOfBirth,
-        string address,
-        string phoneNumber,
-        AdminLevel adminLevel)
-    {
-        return new Admin(userId, email, passwordHash, firstName, lastName,
-            dateOfBirth, address, phoneNumber, adminLevel);
+        public static Admin CreateAdmin(
+            string userId,
+            string email,
+            string passwordHash,
+            string firstName,
+            string lastName,
+            DateTime dateOfBirth,
+            string address,
+            string phoneNumber,
+            AdminLevel adminLevel)
+        {
+            return new Admin(userId, email, passwordHash, firstName, lastName,
+                dateOfBirth, address, phoneNumber, adminLevel);
+        }
     }
-}
 }
